cdaa4db31826f61169afcd3ea9541dfd7123babc
pip10127 <57880328+pip10127@users.noreply.github.com>
2024-03-31 01:11:29 -0600

    Create startup-banner.zsh
    simple banner for the startup of your terminal made for zsh

---
+++ startup-banner.zsh
@@ -0,0 +1,22 @@
+# Auto-start for terminal banner
+# add source to this file to .zshrc
+# default color for the banner is red 
+
+# add your ascii logo here
+echo -e "\e [0;31m
+██████╗  ██╗██████╗ 
+██╔══██╗███║██╔══██╗
+██████╔╝╚██║██████╔╝
+██╔═══╝  ██║██╔═══╝ 
+██║      ██║██║     
+╚═╝      ╚═╝╚═╝                        
+"
+# To change the color for the banner changer the variables 0;31 to a diffrent number
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
